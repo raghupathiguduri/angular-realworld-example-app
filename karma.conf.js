3c4c45b38184001b5ec328b0336f77460609ec20
Raghupathi <raghupathiguduri@gmail.com>
2022-06-16 11:46:20 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,6 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-phantomjs-launcher'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -29,7 +28,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome','PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
